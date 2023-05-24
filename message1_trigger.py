f0d7d8953b07679b00028d47f354a20be0f51c18
NexusRanger <130080367+NexusRanger@users.noreply.github.com>
2023-05-24 15:52:13 +0100

    Update message1_trigger.py

--- message1_trigger.py
+++ message1_trigger.py
@@ -2,7 +2,7 @@
 import subprocess
 
 python_path = r"C:\Program Files\Python311\python.exe"
-script_path = r"c:\data\python\VoiceFiles\message2_say_or_fetch_test.py"
+script_path = r"c:\data\python\VoiceFiles\message2_say_or_fetch_run.py"
 
 # Run the script using the subprocess module
 subprocess.run([python_path, script_path])
